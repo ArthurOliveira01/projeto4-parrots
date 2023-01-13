908ea0326a768d4e9df931bc18973f43c3f8e448
Arthur Oliveira da Silva <e.arthur.osilva@mpt.mp.br>
2023-01-13 13:18:17 -0400

    Added images folders and updating the layout

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,8 @@
 function inicioJogo(){
-    
+    nCartas = prompt('Digite uma quantidade par de cartas de 4 a 14');
+    if(nCartas > 3 && nCartas < 15 && nCartas % 2 == 0){
+        alert('Quantidade de cartas válida');
+    } else{
+        alert('Quantidade de cartas inválida');
+    }
 }
