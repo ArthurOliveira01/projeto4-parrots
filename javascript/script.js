d2894f2b11fb9eca7a0fb8cf16287129838f23e1
Arthur Oliveira da Silva <e.arthur.osilva@mpt.mp.br>
2023-01-17 15:45:53 -0400

    Adicionando lógica do jogo

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,61 @@
-function inicioJogo(){
-    nCartas = prompt('Digite uma quantidade par de cartas de 4 a 14');
-    if(nCartas > 3 && nCartas < 15 && nCartas % 2 == 0){
-        alert('Quantidade de cartas válida');
-    } else{
-        alert('Quantidade de cartas inválida');
+
+const verso1 = {id: 0, name: "messi"}
+const verso2 = {id: 0, name: "messi"}
+const verso3 = {id: 1, name: "cat"}
+const verso4 = {id: 1, name: "cat"}
+const verso5 = {id: 2, name: "terra"}
+const verso6 = {id: 2, name: "terra"}
+const verso7 = {id: 3, name: "troy"}
+const verso8 = {id: 3, name: "troy"}
+const verso9 = {id: 4, name: "dumbledore"}
+const verso10 = {id: 4, name: "dumbledore"}
+const verso11 = {id: 5, name: "hxh"}
+const verso12 = {id: 5, name:"hxh" }
+const verso13 = {id: 6, name: "erwin"}
+const verso14 = {id: 6, name: "erwin"}
+
+const todas = [verso1, verso2, verso3, verso4, verso5, verso6, verso7, 
+    verso8, verso9, verso10, verso11, verso12, verso13, verso14];
+
+function timer(tempo){
+    setInterval(function(){
+        document.getElementById("timer").innerHTML = tempo.toString();
+        tempo +=1;
+    }, 2000);
+}
+function game(){
+    let qCartas = 0;
+    let rodadas = 0;
+    let tempo = 0;
+    while(qCartas % 2 !== 0|| qCartas > 14 || qCartas < 4 ){
+        qCartas = prompt('Qual a quantidade de cartas você quer que o jogo tenha? (limitado a um número par e entre 4 e 14');
     }
+    timer(tempo);
+    addCartas(qCartas);
 }
 
-function addCartas(){
-    let aux = 0;
-    while(aux < nCartas){
-        //adicionar lógica do código de adicionar as divs de carta
-    }
+function virarCarta(){
+    console.log('clique')
+}
+
+
+function addCartas(qCartas){
+    const jogo = document.querySelector('.jogo');
+    const atual = todas.slice(0, qCartas).sort((id, name) => 0.5 - Math.random());
+    jogo.innerHTML = "";4
+    atual.forEach(card => {
+        jogo.innerHTML +=
+        `<div class="maincontainer" onclick="clickCard(this, ${card.id})">
+        <div class="card" data-test="card">
+            <div class="frente">
+                <img src="imagens/back.png" alt="" />
+            </div>
+            <div class="costas">
+                <img src="imagens/${card.name}.gif" alt=""/>
+            </div>
+        </div>
+    </div>`
+    })
 }
 
 function turnCarta(){
@@ -22,10 +66,8 @@ function checkMatch(){
     //Verificar se as cartas quando comparadas possuem o mesmo gif, caso possuirem, se mantém viradas e é contabilizada uma jogada, caso sejam diferentes elas viram de novo e é contabilizada a jogada
 }
 
-function timer(){
-    //contabiliza em quanto tempo o jogador finaliza o jogo
+function win(){
+    
 }
 
-function win(){
-    //função que verifica se a quantidade de acertos é o suficiente para encerrar o jogo, caso seja encerre o jogo e retorne a mensagem de em quantas jogadas, quanto tempo o jogador demora para finalizar o jogo, 
-}
+game();
